799c43dc679052df0baeb786db3ae7bc36c74790
Volodymyr Morhun <your.email@example.com>
2025-06-01 19:06:23 +0300

    phone_number

--- phone_number.py
+++ phone_number.py
@@ -6,4 +6,5 @@ def normalize_phone(phone_number):
     if not need_number.startswith('38'):
       need_number="38"+ need_number
     return "+" + need_number
+
 print(normalize_phone("    38050-111-22-22"))
