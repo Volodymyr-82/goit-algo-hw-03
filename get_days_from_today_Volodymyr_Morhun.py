b553c3f013cf782b6617b8b22a34e361f26e332c
Volodymyr-82 <vo82va@gmail.com>
2025-06-01 13:33:50 +0300

    Add files via upload

--- get_days_from_today_Volodymyr_Morhun.py
+++ get_days_from_today_Volodymyr_Morhun.py
@@ -1,9 +1,12 @@
 from datetime import datetime 
 
 def get_days_from_today(date):
-  date_dt=datetime.strptime(date, "%Y-%m-%d").date()
-  today=datetime.today().date()
-  num_between_days=today-date_dt
-  return num_between_days.days
-
-print(get_days_from_today("2021-10-09"))
+  try:
+    date_dt=datetime.strptime(date, "%Y-%m-%d").date()
+  
+    today=datetime.today().date()
+    num_between_days=today-date_dt
+    return num_between_days.days
+  except ValueError:
+    print(f'{date} — невірний формат дати (потрібно вводити YYYY-MM-DD)')
+print(get_days_from_today("2021-11-09"))
--- get_numbers_ticket(min, max, quantity.py
+++ get_numbers_ticket(min, max, quantity.py
@@ -1,10 +1,15 @@
 import random
 
 def get_numbers_ticket(min, max, quantity):
-    lottery_numbers=[]
-    while len(lottery_numbers)<quantity:
-        num=random.randint(min, max)
-        lottery_numbers.append(num)
-    return lottery_numbers
+    if min>max:
+        print("Помилка: мінімум більший за максимум.")
+        return []
+    elif quantity > (max- min+ 1):
+        print("Помилка: запрошено більше унікальних чисел, ніж можливо.")
+        return []
+    else:
+      lottery_numbers=random.sample(range(min, max + 1), quantity)
+      lottery_numbers.sort()
+      return lottery_numbers
 lottery_numbers = get_numbers_ticket(1, 49, 6)
 print(lottery_numbers)
